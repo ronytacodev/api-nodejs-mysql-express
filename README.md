536bb21e7d726f1ef248812d2e924680b883548d
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-05 10:07:27 -0500

    install nodemon, and start server

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Commands 
 * npm init -y 
 * npm install express
+* node server. js (for start server 1st option)
+* npm i nodemon --save-dev (2nd option)
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+app.set('port',process.env.PORT || 9000)
+
+app.get('/', (req, res) => {
+    res.send('welcome to my API')
+})
+
+app.listen(app.get('port'), () => {
+    console.log('server running on port', app.get('port'))
+})
