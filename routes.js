68aacd827f593973628811f5835dca7abf2e2f8a
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-04-06 08:24:26 -0500

    create methos delete, update

--- routes.js
+++ routes.js
@@ -25,4 +25,28 @@ routes.post('/', (req, res)=>{
     })
 })
 
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+        // console.log(req.body)
+        conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book deleted')
+        })
+    })
+})
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn) => {
+        if(err) return res.send(err)
+        // console.log(req.body)
+        conn.query('UPDATE books SET ? WHERE id = ?', [req.body, req.params.id], (err, rows)=>{
+            if(err) return res.send(err)
+
+            res.send('book updated')
+        })
+    })
+})
+
 module.exports = routes
